f234715cf3c2cf6e60a5abb84da47249b7ea01ae
Rodr1goPA <pereira87pa@gmail.com>
2023-10-14 15:55:34 -0300

    Update
    Fiz algumas melhorias visuais. Foram alterados os arquivos index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,79 +1,70 @@
-        let tableVisible = false;
-        async function postJSON(event) {
-            const formData = new FormData(document.querySelector('form'));
-            const firstname = formData.get("firstname");
-            const lastname = formData.get("lastname");
-            const email = formData.get("email");
-            const idade = formData.get("idade");
-            const sexo = formData.get("sexo");
-            try {
-                const response = await fetch("https://crudcrud.com/api/b829e2fc15d1446a849250aff31c6d91/dadosUser", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        firstname: firstname,
-                        lastname: lastname,
-                        email: email,
-                        idade: idade,
-                        sexo: sexo
-                    }),
-                });
-                const data = await response.json();
-                console.log('Success:', data);
-            } catch (error) {
-                console.error("Error:", error);
-            }
-            const form = event.target;
-            form.reset();
-            await getJSON();
-        }
+async function postJSON(event) {
+    const formData = new FormData(document.querySelector('form'));
+    const firstname = formData.get("firstname");
+    const lastname = formData.get("lastname");
+    const email = formData.get("email");
+    const idade = formData.get("idade");
+    const sexo = formData.get("sexo");
+    try {
+        const response = await fetch("https://crudcrud.com/api/8db58b49b9c840b99f0c6f4dc933bf1d/usuarios", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                idade: idade,
+                sexo: sexo
+            }),
+        });
+        const data = await response.json();
+        console.log('Success:', data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+    const form = event.target;
+    form.reset();
+}
 
-        async function getJSON() {
-            try {
-                const response = await fetch("https://crudcrud.com/api/b829e2fc15d1446a849250aff31c6d91/dadosUser", {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
+async function getJSON() {
+    try {
+        const response = await fetch("https://crudcrud.com/api/8db58b49b9c840b99f0c6f4dc933bf1d/usuarios", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
 
-                if (response.ok) {
-                    const data = await response.json();
-                    console.log('Dados obtidos com sucesso:', data);
-                    populateTable(data);
-                    toggleTable()
-                } else {
-                    console.error('Erro ao obter dados. Status:', response.status);
-                }
-            } catch (error) {
-                console.error("Erro na solicitação:", error);
-            }
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Dados obtidos com sucesso:', data);
+            populateTable(data); /*alteração*/
+        } else {
+            console.error('Erro ao obter dados. Status:', response.status);
         }
-        
-        function toggleTable() {
-            const table = document.getElementById('data-table');
-            table.style.display = tableVisible ? 'none' : 'table';
-            tableVisible = !tableVisible;
+    } catch (error) {
+        console.error("Erro na solicitação:", error);
+    }
+}
+function populateTable(data) {
+    const headTable = document.getElementById('head-table');
+    headTable.innerHTML = `
+    <th>ID</th>
+    <th>Nome</th>
+    <th>Sobrenome</th>
+    <th>Email</th>
+    <th>Idade</th>
+    <th>Sexo</th>
+    `;
+    const tableBody = document.getElementById('data-body');
+    tableBody.innerHTML = ''; // Limpa o conteúdo anterior da tabela
+    data.forEach(item => {
+        const row = tableBody.insertRow();
+        for (key in item) {
+            const cell = row.insertCell();
+            cell.innerHTML = item[key];
         }
-        function populateTable(data) {
-            const tableBody = document.getElementById('data-body');
-            tableBody.innerHTML = '';
-            data.forEach(item => {
-                const row = tableBody.insertRow();
-                for (key in item) {
-                    const cell = row.insertCell();
-                    cell.innerHTML = item[key];
-                }
-            });
-        }
-        function toggleTable() {
-            const table = document.getElementById('data-table');
-            if (tableVisible) {
-                table.style.display = 'none';
-            } else {
-                table.style.display = 'table';
-            }
-            tableVisible = !tableVisible; 
-        }
+    });
+}
